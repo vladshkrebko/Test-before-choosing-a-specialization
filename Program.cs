066e076a420cf917579a99cf90c57c961f1383fa
vladshkrebko <vladshkrebko@gmail.com>
2022-06-19 16:39:10 +0300

    Добавлен код задачи

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+Console.Clear();
+
+Console.WriteLine("Введи элементы массива через пробел:");
+string[] array = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+string[] resultArray = GetLenSelectedArray(array, 3);
+
+Console.WriteLine(string.Join(", ", resultArray));
+
+string[] GetLenSelectedArray(string[] arr, int charNumber)
+{
+    string[] result = new string[arr.Count(x => x.Length <= charNumber)];
+    int count = 0;
+    foreach (string el in arr)
+        if (el.Length <= charNumber)
+        {
+            result[count] = el;
+            count++;
+        }
+    return result;
+}
